104fe4cba80a1ffff5775d862f29cc07299e5c7c
eddielo <eddielo106@gmail.com>
2018-07-20 21:19:39 -0700

    save

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -17,7 +17,7 @@ var parseJSON = function(json) {
     console.log('obj-- ' + json)
     var obj = {};
 
-    json = json.split(':').map(getVal);
+    json = json.split(':').map(cleanElem);
     console.log(json)
 
     return obj;
@@ -27,6 +27,40 @@ var parseJSON = function(json) {
   }
 
   var getVal = function(value) {
+    if (typeof value === 'string') {
+      if (value === undefined) {
+        value = '';
+      } else if (value.includes('true')) {
+        value = true;
+      } else if (value.includes('false')) {
+        value = false;
+      } else if (value.includes('null')) {
+        value = null;
+      } else if (value.search(/\d/) >= 0) {
+        value = Number(value);
+      } else if (value.includes('\\')) {
+        value = removeBackslash(value);
+      }
+    }
+    return value;
+  }
+
+  var cleanElem = function(elem) {
+    var seperators = ['{', '}', '"', '[', ']'];
+    console.log(elem)
+    return elem.split('').filter(function(char) {
+      return char.includes(new RegExp(seperators.join('|') + 'g'));
+    }).join('');
+  }
+
+  var removeBackslash = function(elem) {
+    chars = elem.split('')
+    for (var i = 0; i < chars.length; i++) {
+      if (chars[i] === '\\') {
+        chars.splice(i, 1);
+      }
+    }
+    return chars.join('');
   }
 
   if (json.indexOf('[') < json.indexOf('{') && json.includes('[') || !json.includes('{')) {
