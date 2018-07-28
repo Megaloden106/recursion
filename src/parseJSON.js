abdea9795ff47909c502f58f1cf5e9afeef055e3
eddielo <eddielo106@gmail.com>
2018-07-27 19:09:37 -0700

    Start on nesting portion

--- src/parseJSON.js
+++ src/parseJSON.js
@@ -24,7 +24,42 @@ var parseJSON = function(json) {
   }
 
   var combineNests = function(array) {
+    var result = [];
+    var end = 0, count = 0;
     console.log(array);
+    // for each element in array, look for brackets
+    for ( var i = 0; i < array.length; i++ ) {
+      if ( array[i].search(/{|\[/g) >= 0 ) {
+        // if brackets, start is current index and find end
+        for ( var j = i; j < array.length; j++ ) {
+          for ( var char of array[j] ) {
+            if ( char === '[' || char === '{' ) {
+              count++;
+            } else if ( char === ']' || char === '}' ) {
+              count--;
+              if ( count === 0 ) {
+                end = j;
+                break;
+              }
+            }
+          }
+        }
+        // concat nested elements
+        var nestedJSON = '';
+        console.log(i)
+        console.log(end)
+
+        // splice start to end with the parsed nested element
+        // array.splice(i, end-i+1, parseJSON());
+
+        // stay on current index for nested loops
+        // i--;
+      } else {
+        // push element to result
+        result.push(elem);
+      }
+    }
+
     return array;
   }
 
@@ -44,9 +79,11 @@ var parseJSON = function(json) {
     var result = {};
     if ( json.length > 2 ) {
       var props = json.slice(1,-1).split(/,(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)|:/g);
-      if ( json.slice(1,-1).search(/{|\[/g) >= 0 ) {
+
+      if ( json.search(/{|\[/g) >= 0 ) {
         props = combineNests(props);
       }
+
       for ( var i = 0; i < props.length; i+=2 ) {
         result[eval(props[i])] = eval(props[i+1]);
       }
@@ -55,3 +92,20 @@ var parseJSON = function(json) {
   }
 
 };
+
+
+  var checkNesting = function(array) {
+    var totalNests = findCurrLevNests(array);
+
+    while (totalNests > 0) {
+      var range = findNestIndices(array);
+      var nestedJSON = concatNestedJSON(range, array);
+      if (range.bracket === '{') {
+        array.splice(range.start, range.end - range.start + 1, parseObj(nestedJSON));
+      } else {
+        array.splice(range.start, range.end - range.start + 1, parseArray(nestedJSON));
+      }
+      totalNests--;
+    }
+    return array;
+  }
